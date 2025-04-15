c752140c55212092b379a6502d4d9b7e30e9d789
Priyanshu Agarwal <priyanshuagarwal180311@gmail.com>
2025-04-15 21:33:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === index) ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+// Start slideshow
+showSlide(currentSlide);
+setInterval(nextSlide, 4000); // Change slide every 4 seconds
